2ae4febd6a0f3b9197b8c49bfd893ff3ad0e9ceb
Youssef <53302867+youssefld@users.noreply.github.com>
2021-05-30 16:35:30 +0200

    Integraciones iniciales de los compañeros

---
+++ src/frontend/fire_api/integrations/AirPollutionGraph.svelte
@@ -0,0 +1,26 @@
+<script>
+
+async function loadData(){
+    const API_URL = "https://sos2021-03.herokuapp.com/api/integrations/air-pollution/"
+    let res = await fetch(API_URL)
+    let airPollutionData = await res.json()
+    if(res.ok){
+        console.log("Se han cargado los datos correctamente")
+        console.log(airPollutionData)
+    }else{
+        console.log("No se han podido cargar los datos")
+    }
+}
+
+loadData();
+
+</script>
+
+<svelte:head>
+
+</svelte:head>
+
+<main>
+    
+
+</main>
---
+++ src/frontend/fire_api/integrations/SmokingGraph.svelte
@@ -0,0 +1,23 @@
+<script>
+async function loadData(){
+    const API_URL = "https://sos2021-11.herokuapp.com/api/v2/smoking_stats"
+    let res = await fetch(API_URL)
+    let smokingData = await res.json()
+    if(res.ok){
+        console.log("Se han cargado los datos correctamente")
+        console.log(smokingData)
+    }else{
+        console.log("No se han podido cargar los datos")
+    }
+}
+
+loadData();
+</script>
+
+<svelte:head>
+
+</svelte:head>
+
+<main>
+    
+</main>
