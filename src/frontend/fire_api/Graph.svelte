f74cc3330f90eed43474a43b56824ea24de7af8b
Youssef Lakouifat <youssef@MacBook-Pro-de-Youssef.local>
2021-06-05 01:06:30 +0200

    Se ha actualizado la grafica de Highcharts

--- src/frontend/fire_api/Graph.svelte
+++ src/frontend/fire_api/Graph.svelte
@@ -4,10 +4,11 @@
     } from "svelte";
  
     let fireData = [];
-    let ejeX = [];
-    let nfc = [];
-    let aee = [];
-    let nvs = [];
+    let graphData1 = [];
+    let graphData2 = [];
+    let graphData3 = []
+
+
     
     async function loadGraph(){  
         const res = await fetch("/api/v2/fire-stats");
@@ -15,71 +16,91 @@
             fireData = await res.json();
             console.log(JSON.stringify(fireData, null, 2))
             fireData.forEach(data => {
-                ejeX.push(data.country + "-" + data.year);
-                nfc.push(data["fire_nfc"]);
-                aee.push(data["fire_aee"]);
-                nvs.push(data["fire_nvs"]);
+                let country_year = data.country + "-" + data.year;
+                let nfc = data["fire_nfc"];
+                let aee = data["fire_aee"];
+                let nvs = data["fire_nvs"]
+                graphData1.push([country_year, nfc])
+                graphData2.push([country_year, aee])
+                graphData3.push([country_year, nvs])
+
             });
-        }else{
+        } else {
             console.log("Error loading fires");
         }
-
+        console.log("Generando grafo con los siguientes datos:")
         Highcharts.chart('container', {
+            chart: {
+                type: 'area',
+                options3d: {
+                    enabled: true,
+                    alpha: 15,
+                    beta: 30,
+                    depth: 200
+                }
+            },
             title: {
-                text: 'Gráfico de incendios'
+                text: "Estadísticas de incendios, emisiones y especies vegtales"
             },
             yAxis: {
                 title: {
-                    text: ''
-                }
-            },
-            xAxis: {
-                title: {
-                    text: 'País-Año'
+                    text: 'Número de unidades',
+                    x: -80
                 },
-                categories: ejeX
+                labels: {
+                    format: '{value:,.0f} unidades'
+                },
+                gridLineDashStyle: 'Dash'
             },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle'
-            },
-            series: [{
-                name: 'Incendios',
-                data: nfc
-            },
-            {
-                name: 'Estmación anual de emisiones',
-                data: aee
-            },
-            {
-                name: 'Especies vegetales afectadas',
-                data: nvs
+            xAxis: [{
+                visible: false
+            }, {
+                visible: false
+            }, {
+                visible: false
             }],
-            responsive: {
-                rules: [{
-                    condition: {
-                        maxWidth: 500
+            plotOptions: {
+                area: {
+                    depth: 100,
+                    marker: {
+                        enabled: false
                     },
-                    chartOptions: {
-                        legend: {
-                            layout: 'horizontal',
-                            align: 'center',
-                            verticalAlign: 'bottom'
+                    states: {
+                        inactive: {
+                            enabled: false
                         }
                     }
-                }]
-            }
+                }
+            },
+            tooltip: {
+                valueSuffix: ' unidades'
+            },
+            series: [{
+                name: "Incendios",
+                lineColor: 'rgb(180,90,50)',
+                color: 'rgb(200,110,50)',
+                fillColor: 'rgb(200,110,50)',
+                data: graphData1
+            }, {
+                name: "Emisiones emitidas",
+                lineColor: 'rgb(230,90,50)',
+                color: 'rgb(230,90,50)',
+                fillColor: 'rgb(230,90,50)',
+                data: graphData2
+            }, {
+                name: "Especies vegetales",
+                lineColor: 'rgb(87,213,29)',
+                color: 'rgb(80,200,20)',
+                fillColor: 'rgb(80,200,20)',
+                data: graphData3
+            }]
         });
-  }
+    }
 </script>
 
 <svelte:head>
     <script src="https://code.highcharts.com/highcharts.js" on:load="{loadGraph}"></script>
-    <script src="https://code.highcharts.com/modules/series-label.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/modules/export-data.js"></script>
-    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+    <script src="https://code.highcharts.com/highcharts-3d.js"></script>
 </svelte:head>
 
 <main>
