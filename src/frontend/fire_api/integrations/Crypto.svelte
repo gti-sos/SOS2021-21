05955d6183208a7d73d56c4d5a407bc00813d0fb
Youssef Lakouifat <youssef@MacBook-Pro-de-Youssef.local>
2021-06-07 21:34:41 +0200

    APIS incluidas

--- src/frontend/fire_api/integrations/Crypto.svelte
+++ src/frontend/fire_api/integrations/Crypto.svelte
@@ -1,53 +1,51 @@
 
 <script>
+    import { Form, FormGroup, FormText, Input, Label } from 'sveltestrap';
     import Button from "sveltestrap/src/Button.svelte";
-    import Table from "sveltestrap/src/Table.svelte";
-    let dataTable = [];
-
-    async function loadData() {
-        const URL = "https://blockchain.info/ticker"
+    let tx = ''
+    let bloque = ''
+    let indice = ''
+    let talla = ''
+    async function loadData(tx) {
+        console.log("Fetching API...")
+        const URL = "https://blockchain.info/rawtx/"+tx
         const res = await fetch(URL)
         console.log("Se ha realizado una llamada correctamente");
         let data = await res.json()
-        for (let [key, value] of Object.entries(data)) {
-            dataTable.push([key]);
-        }
+        bloque = data.block_height
+        indice = data.tx_index
+        talla = data.size
+
     }
 
 </script>
 
 <main>
-    <h1>Precio del Bitcoin en diferentes divisas</h1>
+    <h1>Información sobre una transaccion de Bitcoin</h1>
     <hr>
-    {#await dataTable}
-        Cargando los datos
-    {:then dataTable}
-    <Table responsive>
-        <thead>
-            <tr>
-                <th>Cambio</th>
-                <th>15m</th>
-                <th>Ultimo precio</th>
-                <th>Precio de compra</th>
-                <th>Precio de venta</th>
-                <th>Simbolo</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each dataTable as c}
-                <tr>
-                    <td>{c[0]}</td>
-                    <td>{c[0]}</td>
-                    <td>{c[0]}</td>
-                    <td>{c[0]}</td>
-                    <td>{c[0]}</td>
+    <Form>
+        <FormGroup>
+          <Label for="exampleTx">Ejemplo: b6f6991d03df0e2e04dafffcd6bc418aac66049e2cd74b80f14ac86db1e3f0da</Label>
+        </FormGroup>
+        <FormGroup>
+            <Label for="tx">Introduce una transaccion:</Label>
+            <Input type="text" bind:value={tx} />
+        </FormGroup>
+        <FormGroup>
+            <Label for="tx">Tamaño del bloque:</Label>
+            <Input type="text" bind:value={bloque} readonly={true}/>
+        </FormGroup>
+        <FormGroup>
+            <Label for="tx">Indice de transaccion</Label>
+            <Input type="text" bind:value={indice} readonly={true}/>
+        </FormGroup>
+        <FormGroup>
+            <Label for="tx">Talla (bytes)</Label>
+            <Input type="text" bind:value={talla} readonly={true}/>
+        </FormGroup>
+        <Button color="primary" on:click="{loadData(tx)}">Buscar</Button>
 
-                </tr>
-            {/each}
-        </tbody>
-    </Table>
-    
-    <hr>
-    <Button color="primary" on:click={loadData}>Obtener precios</Button>
-{/await}
+
+    </Form>
+
 </main>
